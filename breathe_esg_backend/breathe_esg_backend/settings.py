d41b252dcb023ce0a5c487f01a9a0e9648906d20
Bedangshu Raj Mudiar <bedangshurajmudiar@Bedangshus-MacBook-Air.local>
2026-05-28 14:03:10 +0530

    Add production deployment configurations (gunicorn, whitenoise, build.sh)

--- breathe_esg_backend/breathe_esg_backend/settings.py
+++ breathe_esg_backend/breathe_esg_backend/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # Prepend for proper header responses
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # Disable CsrfViewMiddleware to allow smooth REST API testing for the prototype
@@ -131,6 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Enable WhiteNoise compression and caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
